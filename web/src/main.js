196e1569e3828a3c156203ec68d59eae79ec7d60
cramk <unknown@unknown>
2021-03-19 00:03:25 -0400

    Update main.js

--- web/src/main.js
+++ web/src/main.js
@@ -1,23 +1,23 @@
-import {createApp} from 'vue';
-import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/saga-blue/theme.css'       //theme
-import 'primevue/resources/primevue.min.css'                 //core css
-import 'primeicons/primeicons.css'                           //icons
-import 'primeflex/primeflex.css';
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import Dialog from 'primevue/dialog';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
-import Panel from 'primevue/panel';
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import Panel from "primevue/panel";
 
 const app = createApp(App);
 
 app.use(PrimeVue);
-app.use(VueAxios, axios)
-app.component('Dialog', Dialog);
-app.component('Button', Button);
-app.component('InputText', InputText);
-app.component('Panel', Panel);
+app.use(VueAxios, axios);
+app.component("Dialog", Dialog);
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.component("Panel", Panel);
 app.mount("#app");
